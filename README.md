7f88fcba0e97da37b828e2b66bd67e3ced6aeb30
sunxiaoyu <sunxiaoyu.space@outlook.com>
2022-01-05 10:47:12 +0800

    update

---
+++ README.md
@@ -0,0 +1,50 @@
+# strapi-crack-ee
+
+**WARNING: This project is only for technique testing, DO NOT use this package in any production environment!**
+
+Usage:
+
+1. Crack when install the package
+
+1.1. Make sure you have install the required packages (Strapi packages) already.
+
+``` shell
+npm install
+```
+
+1.2. Install in your strapi project by below command:
+
+```shell
+npm install @iopanda/strapi-crack-ee
+```
+
+This package will generate the license automatically in your project.
+
+2. Crack by command
+
+Also, you can run the crack by command.
+
+2.1 Install packages
+
+``` shell
+npm install
+npm install @iopanda/strapi-crack-ee --save
+```
+
+2.2 Modify your package.json file, add new record in script
+
+``` json
+{
+    "scripts": {
+        "crack": "strapi-crack"
+    }
+}
+```
+
+2.3 Run command by npm
+
+```shell
+npm run crack
+```
+
+Good Luck!
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+#!/usr/bin/env node
 const { createSign, createVerify } = require("crypto")
 const fs = require("fs")
 const { cwd } = require('process')
 const path = require('path')
 
-const privateKey = fs.readFileSync("./certs/private.pem").toString()
-const publicKey = fs.readFileSync("./certs/key.pub").toString()
-const data = fs.readFileSync("./data/lic.json").toString()
+const privateKey = fs.readFileSync(path.join(__dirname, "certs/private.pem")).toString()
+const publicKey = fs.readFileSync(path.join(__dirname, "certs/key.pub")).toString()
+const data = fs.readFileSync(path.join(__dirname, "data/lic.json")).toString()
 
 const sign = createSign("RSA-SHA256")
 sign.write(data)
@@ -15,5 +16,16 @@ const signature64 = sign.sign(privateKey).toString("base64")
 const data64 = Buffer.from(data).toString("base64")
 
 const license = Buffer.from(`${signature64}\n${data64}`).toString("base64")
-const fp = path.join(cwd(), 'license.txt')
+
+let fp = null
+let fx = null
+if(__dirname == cwd() && process.argv[2] == 'init'){
+    fp = path.join(__dirname, "../../../license.txt")
+    fx = path.join(__dirname, "../../../node_modules/@strapi/strapi/lib/utils/resources/key.pub")
+}else{
+    fp = path.join(cwd(), 'license.txt')
+    fx = path.join(cwd(), 'node_modules/@strapi/strapi/lib/utils/resources/key.pub')
+}
 fs.writeFileSync(fp, license)
+fs.writeFileSync(fx, publicKey)
+console.log(`-> LICENSE saved to ${fp}`)
